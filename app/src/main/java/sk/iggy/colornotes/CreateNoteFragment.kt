1bc1e45af304a185ce758ebea2dc8b74ca7422e4
iggy228 <i.borovy@gmail.com>
2021-04-06 16:53:10 +0200

    creating basic of note app.

---
+++ app/src/main/java/sk/iggy/colornotes/CreateNoteFragment.kt
@@ -0,0 +1,31 @@
+package sk.iggy.colornotes
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+/**
+ * A simple [CreateNoteFragment] subclass as the second destination in the navigation.
+ */
+class CreateNoteFragment : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_note, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.button_second).setOnClickListener {
+            findNavController().navigate(R.id.CreateNoteFragment_to_NoteFragment)
+        }
+    }
+}
---
+++ app/src/main/java/sk/iggy/colornotes/NoteFragment.kt
@@ -0,0 +1,31 @@
+package sk.iggy.colornotes
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+/**
+ * A simple [NoteFragment] subclass as the default destination in the navigation.
+ */
+class NoteFragment : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_notes, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<FloatingActionButton>(R.id.addButton).setOnClickListener {
+            findNavController().navigate(R.id.NoteFragment_to_CreateNoteFragment)
+        }
+    }
+}
